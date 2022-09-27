fee1f9f9d94e53cf1b02b6e3f889711aae8f79f0
MartinsBalcuns <martins.balcuns@magebit.com>
2022-09-27 12:57:55 +0300

    Initial commit

---
+++ etc/env.php
@@ -0,0 +1,69 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'c28a13f9c558cef486ed60a29b06ce5b'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'hyva_learning',
+                'username' => 'admin',
+                'password' => 'option123',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'c5a_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'c5a_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'downloadable_domains' => [
+        'localhost'
+    ],
+    'cache_types' => [
+        'compiled_config' => 1
+    ],
+    'install' => [
+        'date' => 'Mon, 24 Jan 2022 10:50:54 +0000'
+    ]
+];
